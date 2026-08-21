84083897ff53119382ce1d53f050fd0746097e7d
Jakub <jkxcvj@gmail.com>
2026-08-21 17:01:12 +0200

    Refactor architecture and apply SOLID principles fix

--- include/EventLogger.h
+++ include/EventLogger.h
@@ -6,7 +6,7 @@
 class EventLogger : public EventSubscriber
 {
   public:
-    virtual ~EventLogger() = default;
+    ~EventLogger() override = default;
     void onEvent(EventType event) override { log(toString(event)); }
     virtual void log(std::string_view message) = 0;
 };
